26195ff6632c399542cb3c7773c92007c874a888
Manikandan1818 <simbumanii18@gmail.com>
2023-10-12 18:38:01 +0530

    Readfile, writrfile, append and Renaming file!

--- server.js
+++ server.js
@@ -20,15 +20,64 @@
 // console.log(mul(2, 3));
 // console.log(div(2, 3));
 
-const fs = require("fs");
+const fsPromises = require("fs").promises;
 const path = require("path");
 
-fs.readFile(path.join(__dirname, "files", "test.txt"), "utf-8", (err, data) => {
-  if (err) throw err;
-  console.log(data);
-});
+const fileOps = async () => {
+  try {
+    const data = await fsPromises.readFile(
+      path.join(__dirname, "files", "test.txt"),
+      "utf-8"
+    );
+    console.log(data);
 
-process.on("uncaughtException", (err) => {
-  console.error(`There was an uncaught error: ${err}`);
-  process.exit(1);
-});
+    await fsPromises.writeFile(
+      path.join(__dirname, "files", "createfile.txt"),
+      "File creating using writefile"
+    );
+    console.log("file created!");
+
+    await fsPromises.appendFile(
+      path.join(__dirname, "files", "createfile.txt"),
+      "\n\n Update the file using append command!"
+    );
+    console.log("Append");
+
+    await fsPromises.rename(
+      path.join(__dirname, "files", "createfile.txt"),
+      path.join(__dirname, "files", "renameFile.txt")
+    );
+    console.log("File rename");
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+fileOps();
+
+// fs.readFile(path.join(__dirname, "files", "test.txt"), "utf-8", (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+// });
+
+// fs.writeFile(
+//   path.join(__dirname, "files", "newFile.txt"),
+//   "Hello Node JS",
+//   (err) => {
+//     if (err) throw err;
+//     console.log("Write Compleet");
+//     fs.appendFile(
+//       path.join(__dirname, "files", "newFile.txt"),
+//       "\n\n Thank you",
+//       (err) => {
+//         if (err) throw err;
+//         console.log("Append complete");
+//       }
+//     );
+//   }
+// );
+
+// process.on("uncaughtException", (err) => {
+//   console.error(`There was an uncaught error: ${err}`);
+//   process.exit(1);
+// });
